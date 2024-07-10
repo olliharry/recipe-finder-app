ecb413b6a0b62cd5da82adaaddd2a0392cc0a4a2
psyjv2 <psyjv2@exmail.nottingham.ac.uk>
2024-07-10 17:17:40 +0100

    added auth

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,29 +1,147 @@
 import React from "react";
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await auth();
+
   return (
-    <div className="navbar bg-primary shadow-lg">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+    <div className="drawer ">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col bg-pr">
+        <div className="navbar bg-primary w-full">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="mx-2 flex-1 px-2 text-2xl font-extrabold">
+            Recipe Radar
+          </div>
+          <div className="hidden flex-none lg:block">
+            <ul className="menu menu-horizontal">
+              {!session?.user && (
+                <li>
+                  <form
+                    action={async () => {
+                      "use server";
+                      await signIn();
+                    }}
+                  >
+                    <button type="submit" className=" text-xl font-extrabold">
+                      Sign in
+                    </button>
+                  </form>
+                </li>
+              )}
+              {session?.user && (
+                <li>
+                  <details>
+                    <summary>
+                      <p className="text-xl font-extrabold">
+                        {session?.user?.name}
+                      </p>
+                      {session?.user?.image && (
+                        <Image
+                          src={session?.user?.image}
+                          alt={"profile Picture"}
+                          width={40}
+                          height={40}
+                          className="rounded-full"
+                        />
+                      )}
+                    </summary>
+
+                    <ul className="bg-primary rounded-t-none p-2">
+                      <li>
+                        <form
+                          action={async () => {
+                            "use server";
+                            await signOut();
+                          }}
+                        >
+                          <button
+                            type="submit"
+                            className="text-xl font-extrabold"
+                          >
+                            Sign out
+                          </button>
+                        </form>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Link</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+          {!session?.user && (
+            <li>
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn();
+                }}
+              >
+                <button type="submit">Sign in</button>
+              </form>
+            </li>
+          )}
+          {session?.user && (
+            <li>
+              <details>
+                <summary>
+                  {session?.user?.name}
+                  {session?.user?.image && (
+                    <Image
+                      src={session?.user?.image}
+                      alt={"profile Picture"}
+                      width={40}
+                      height={40}
+                      className="rounded-full"
+                    />
+                  )}
+                </summary>
+
+                <ul className="bg-base-200 rounded-t-none p-2">
+                  <li>
+                    <form
+                      action={async () => {
+                        "use server";
+                        await signOut();
+                      }}
+                    >
+                      <button type="submit">Sign out</button>
+                    </form>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          )}
         </ul>
       </div>
     </div>
